afddd7cdf97743b2cff51b342009c353253d61ea
Gideon <125488605+GideonJulian@users.noreply.github.com>
2024-12-17 02:41:04 +0100

    explore page added

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,29 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PostCard from "../components/HomeComponents/PostCard";
+import PostData from "../utils/PostData";
 const Explore = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
+    const handleNavigate = (id) => {
+      console.log("Navigating to post with ID:", id);
+      if (id) {
+        navigate(`/${id}`);
+      } else {
+        console.error("Invalid post ID");
+      }
+    };
+    const matchesSearchQuery = (post) => {
+      return (
+        post.erroTitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        post.postTags.some((tag) =>
+          tag.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    };
+  
+    const filteredPosts = PostData.filter((post) => matchesSearchQuery(post));
   return (
     <div className="px-3 md:px-5 mt-[70px]">
       <div>
@@ -29,6 +52,19 @@ const Explore = () => {
             <i className="bi bi-search absolute top-1/2 left-[87%] transform -translate-y-1/2 text-gray-500 text-lg"></i>
           </div>
         </div>
+      </div>
+      <div className="p-3 md:p-8 w-full grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-4 pb-32">
+        {filteredPosts.map((item) => (
+          <PostCard
+            key={item.id}
+            errorImg={item.screenShort}
+            title={item.erroTitle}
+            userImg={item.userProfile}
+            userName={item.username}
+            postTags={item.postTags}
+            onClick={() => handleNavigate(item.id)}
+          />
+        ))}
       </div>
     </div>
   );
